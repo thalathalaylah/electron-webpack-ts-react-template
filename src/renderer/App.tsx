03d38da06e695484a8aa45c620a7c45bbc3436eb
thalathalaylah <fragment.vertex@gmail.com>
2020-08-12 10:13:04 +0900

    [add] 画像読み込み設定

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import icon from './icon.png';
+import './App.css';
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <img src={icon} className="App-logo" alt="logo" />
       <p>Hello Electron and Typescript and React and more world !</p>
     </div>
   );
