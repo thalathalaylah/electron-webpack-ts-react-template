39ea8ed47ee4203d8c142b0101826fab0a580e88
thalathalaylah <fragment.vertex@gmail.com>
2020-08-26 00:55:42 +0900

    [add] React導入

---
+++ src/renderer/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <p>Hello Electron and Typescript and React and more world !</p>
+    </div>
+  );
+};
+
+export default App;
